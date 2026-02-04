adc90f60be5d06836c2d8915b3715f56a370409a
Nivedita <mnive93@gmail.com>
2026-02-04 13:05:29 +0530

    updated to 6.0.0

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     // Binary target
     .binaryTarget(
     name: "HyperSnapSDK",
-    url: "https://hvsdk.s3.ap-south-1.amazonaws.com/ios/release/hypersnapsdk/6.0.0/HyperSnapSDK-6.0.0-XCFramework.zip",
-    checksum: "8c3cf9f9cd31e577c49d97dd5f8789dc1be7249d3b3b68f89473acf7321da55f"
+     url: "https://hvsdk.s3.amazonaws.com/ios/release/hypersnapsdk/6.0.0/HyperSnapSDK.xcframework.zip"
+     checksum: "24cefbd9ab5305dcc90bf1941f5bccd82905a0b21b564e28c5fd822027c015cf"
 ),
 
     // Resource-only SPM target (contains Sources/HyperSnapSDKResources/Resources)
