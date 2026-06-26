cfe46c06bf2250b354625660c49da04de17593cd
Nivedita <nivedita@hyperverge.co>
2026-06-26 15:42:03 +0530

    updated to 6.5.0

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "HyperSnapSDK",
-      url: "https://hvsdk.s3.amazonaws.com/ios/release/hypersnapsdk/6.4.0/HyperSnapSDK.xcframework.zip",
-      checksum: "4e285971719f601177236a5aeb1f4ae6ce86b71827358d70da3d00bec721a1a1"
+      url: "https://hvsdk.s3.amazonaws.com/ios/release/hypersnapsdk/6.5.0/HyperSnapSDK.xcframework.zip"
+      checksum: "f2e7050cb8846347c3faa7bd81230aa60abeea6b76a6d7a473370a08365c6aa8"
     ),
     .target(
       name: "HyperSnapSDKResources",
